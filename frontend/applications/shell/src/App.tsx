5adec05c948a4b911721e4adf1fc291541ec2969
Kevin Sewell <kevin.w.sewell@gmail.com>
2025-03-07 22:55:55 +0200

    build: adding initial shell refine project

---
+++ frontend/applications/shell/src/App.tsx
@@ -0,0 +1,231 @@
+import {
+  Authenticated,
+  AuthProvider,
+  GitHubBanner,
+  Refine,
+} from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import {
+  ErrorComponent,
+  ThemedLayoutV2,
+  ThemedSiderV2,
+  useNotificationProvider,
+} from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
+
+import { useKeycloak } from "@react-keycloak/web";
+import routerBindings, {
+  CatchAllNavigate,
+  DocumentTitleHandler,
+  NavigateToResource,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router";
+import dataProvider from "@refinedev/simple-rest";
+import { App as AntdApp } from "antd";
+import axios from "axios";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
+import { AppIcon } from "./components/app-icon";
+import { Header } from "./components/header";
+import { ColorModeContextProvider } from "./contexts/color-mode";
+import {
+  BlogPostCreate,
+  BlogPostEdit,
+  BlogPostList,
+  BlogPostShow,
+} from "./pages/blog-posts";
+import {
+  CategoryCreate,
+  CategoryEdit,
+  CategoryList,
+  CategoryShow,
+} from "./pages/categories";
+import { Login } from "./pages/login";
+
+function App() {
+  const { keycloak, initialized } = useKeycloak();
+
+  if (!initialized) {
+    return <div>Loading...</div>;
+  }
+
+  const authProvider: AuthProvider = {
+    login: async () => {
+      const urlSearchParams = new URLSearchParams(window.location.search);
+      const { to } = Object.fromEntries(urlSearchParams.entries());
+      await keycloak.login({
+        redirectUri: to ? `${window.location.origin}${to}` : undefined,
+      });
+      return {
+        success: true,
+      };
+    },
+    logout: async () => {
+      try {
+        await keycloak.logout({
+          redirectUri: window.location.origin,
+        });
+        return {
+          success: true,
+          redirectTo: "/login",
+        };
+      } catch (error) {
+        return {
+          success: false,
+          error: new Error("Logout failed"),
+        };
+      }
+    },
+    onError: async (error) => {
+      console.error(error);
+      return { error };
+    },
+    check: async () => {
+      try {
+        const { token } = keycloak;
+        if (token) {
+          axios.defaults.headers.common = {
+            Authorization: `Bearer ${token}`,
+          };
+          return {
+            authenticated: true,
+          };
+        } else {
+          return {
+            authenticated: false,
+            logout: true,
+            redirectTo: "/login",
+            error: {
+              message: "Check failed",
+              name: "Token not found",
+            },
+          };
+        }
+      } catch (error) {
+        return {
+          authenticated: false,
+          logout: true,
+          redirectTo: "/login",
+          error: {
+            message: "Check failed",
+            name: "Token not found",
+          },
+        };
+      }
+    },
+    getPermissions: async () => null,
+    getIdentity: async () => {
+      if (keycloak?.tokenParsed) {
+        return {
+          name: keycloak.tokenParsed.family_name,
+        };
+      }
+      return null;
+    },
+  };
+
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <ColorModeContextProvider>
+          <AntdApp>
+            <DevtoolsProvider>
+              <Refine
+                dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+                notificationProvider={useNotificationProvider}
+                authProvider={authProvider}
+                routerProvider={routerBindings}
+                resources={[
+                  {
+                    name: "blog_posts",
+                    list: "/blog-posts",
+                    create: "/blog-posts/create",
+                    edit: "/blog-posts/edit/:id",
+                    show: "/blog-posts/show/:id",
+                    meta: {
+                      canDelete: true,
+                    },
+                  },
+                  {
+                    name: "categories",
+                    list: "/categories",
+                    create: "/categories/create",
+                    edit: "/categories/edit/:id",
+                    show: "/categories/show/:id",
+                    meta: {
+                      canDelete: true,
+                    },
+                  },
+                ]}
+                options={{
+                  syncWithLocation: true,
+                  warnWhenUnsavedChanges: true,
+                  useNewQueryKeys: true,
+                  projectId: "pEg9pO-r38cFo-T3iwQG",
+                  title: { text: "Refine Project", icon: <AppIcon /> },
+                }}
+              >
+                <Routes>
+                  <Route
+                    element={
+                      <Authenticated
+                        key="authenticated-inner"
+                        fallback={<CatchAllNavigate to="/login" />}
+                      >
+                        <ThemedLayoutV2
+                          Header={Header}
+                          Sider={(props) => <ThemedSiderV2 {...props} fixed />}
+                        >
+                          <Outlet />
+                        </ThemedLayoutV2>
+                      </Authenticated>
+                    }
+                  >
+                    <Route
+                      index
+                      element={<NavigateToResource resource="blog_posts" />}
+                    />
+                    <Route path="/blog-posts">
+                      <Route index element={<BlogPostList />} />
+                      <Route path="create" element={<BlogPostCreate />} />
+                      <Route path="edit/:id" element={<BlogPostEdit />} />
+                      <Route path="show/:id" element={<BlogPostShow />} />
+                    </Route>
+                    <Route path="/categories">
+                      <Route index element={<CategoryList />} />
+                      <Route path="create" element={<CategoryCreate />} />
+                      <Route path="edit/:id" element={<CategoryEdit />} />
+                      <Route path="show/:id" element={<CategoryShow />} />
+                    </Route>
+                    <Route path="*" element={<ErrorComponent />} />
+                  </Route>
+                  <Route
+                    element={
+                      <Authenticated
+                        key="authenticated-outer"
+                        fallback={<Outlet />}
+                      >
+                        <NavigateToResource />
+                      </Authenticated>
+                    }
+                  >
+                    <Route path="/login" element={<Login />} />
+                  </Route>
+                </Routes>
+
+                <RefineKbar />
+                <UnsavedChangesNotifier />
+                <DocumentTitleHandler />
+              </Refine>
+              <DevtoolsPanel />
+            </DevtoolsProvider>
+          </AntdApp>
+        </ColorModeContextProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
