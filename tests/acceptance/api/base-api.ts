21bc1d1225fd2b4cb4a278a0a097fb1ccd60e412
Kevin Sewell <kevin.w.sewell@gmail.com>
2025-03-14 20:32:11 +0200

    ci: adding acceptance test steps to github action workflow

--- tests/acceptance/api/base-api.ts
+++ tests/acceptance/api/base-api.ts
@@ -5,23 +5,45 @@ import https from "https";
 import process from "node:process";
 import querystring from "node:querystring";
 
+function assertDefined<T extends any>(something: T | undefined): T {
+  if (something == undefined) {
+    throw new Error("Should be defined!");
+  }
+  return something;
+}
+
 export class BaseApi {
   protected readonly httpsAgent = new https.Agent({
     rejectUnauthorized: false,
   });
+
+  protected readonly apiV1Url: string = `${assertDefined(process.env.BOOKSTORE_API_URL)}/v1`;
+  protected readonly adminUsername: string = assertDefined(
+    process.env.KEYCLOAK_BOOKSTORE_ADMIN_USERNAME,
+  );
+  protected readonly adminPassword: string = assertDefined(
+    process.env.KEYCLOAK_BOOKSTORE_ADMIN_PASSWORD,
+  );
+  protected readonly clientId: string = assertDefined(
+    process.env.KEYCLOAK_BOOKSTORE_BACKEND_CLIENT_ID,
+  );
+  protected readonly clientSecret: string = assertDefined(
+    process.env.KEYCLOAK_BOOKSTORE_BACKEND_CLIENT_SECRET,
+  );
   protected readonly logger: Logger = logger;
-  protected readonly tokenUrl = `${process.env.KC_BOOKSTORE_REALM_URL}/protocol/openid-connect/token`;
-  protected readonly apiV1Url = `${process.env.KC_BOOKSTORE_BACKEND_API_URL}/v1`;
+  protected readonly tokenUrl: string = assertDefined(
+    `${process.env.KEYCLOAK_BOOKSTORE_REALM_URL}/protocol/openid-connect/token`,
+  );
 
   constructor(protected readonly axiosInstance: AxiosInstance) {}
 
   async getAccessToken() {
     const loginRequest = querystring.stringify({
       grant_type: "password",
-      client_id: process.env.KC_BOOKSTORE_BACKEND_CLIENT_ID,
-      client_secret: process.env.KC_BOOKSTORE_BACKEND_CLIENT_SECRET,
-      username: process.env.KC_BOOKSTORE_BACKEND_ADMIN_USERNAME,
-      password: process.env.KC_BOOKSTORE_BACKEND_ADMIN_PASSWORD,
+      client_id: this.clientId,
+      client_secret: this.clientSecret,
+      username: this.adminUsername,
+      password: this.adminPassword,
     });
 
     const tokenResponse = await axios.post(this.tokenUrl, loginRequest, {
--- tests/acceptance/steps/fixture.ts
+++ tests/acceptance/steps/fixture.ts
@@ -8,7 +8,7 @@ import { SignInPage } from "@pages/sign-in-page";
 import axiosInstance, { AxiosInstance } from "axios";
 import * as dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({path: "../../.env"});
 
 export type Fixtures = {
   axiosInstance: AxiosInstance;
