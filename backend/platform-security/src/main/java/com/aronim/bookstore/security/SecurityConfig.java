a9d9ade22f1967c88ad0a9fdb746fba350cf3569
Kevin Sewell <kevin.w.sewell@gmail.com>
2025-03-14 15:25:02 +0200

    ci: checking pods after install helm chart

--- backend/platform-security/src/main/java/com/aronim/bookstore/security/SecurityConfig.java
+++ backend/platform-security/src/main/java/com/aronim/bookstore/security/SecurityConfig.java
@@ -73,7 +73,8 @@ public class SecurityConfig {
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter))
-                );
+                )
+        ;
 
         return http.build();
     }
